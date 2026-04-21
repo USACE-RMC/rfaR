24823917ac5be2670feb4199720e858477183033
Mukunda Rao Katta <mukunda.vjcs6@gmail.com>
2026-04-21 12:59:43 -0700

    docs(power_function): fix '@param x Highest power of 10 to us' -> 'to use' (#21)
    Closes #10 (partial — just the power_function doc typo)

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 #' Power Function
 #'
 #' Raises 10 to the 1st through the nth power
-#' @param x Highest power of 10 to us
+#' @param x Highest power of 10 to use
 #' @return Vector of 10 raised sequentially from the 1st to the nth power.
 #' @keywords internal
 power_function <- function(x) {
